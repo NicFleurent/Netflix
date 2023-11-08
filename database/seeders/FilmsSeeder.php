c1cd605192385434069d3191c90957d24c526bb9
Nicolas Fleurent <nicolas.fleurent.02@edu.cegeptr.qc.ca>
2023-11-08 14:23:42 -0500

    Correction seeder film - monde charile

--- database/seeders/FilmsSeeder.php
+++ database/seeders/FilmsSeeder.php
@@ -258,7 +258,7 @@ class FilmsSeeder extends Seeder
                 'resume' => "Au lycée où il vient d'arriver, on trouve Charlie bizarre. Sa sensibilité et ses goûts sont en décalage avec ceux de ses camarades de classe. Pour son prof de Lettres, c'est sans doute un prodige, pour les autres, c'est juste un \"loser\". En attendant, il reste en marge - jusqu'au jour où deux terminales, Patrick et la jolie Sam, le prennent sous leur aile. Grâce à eux, il va découvrir la musique, les fêtes, le sexe… pour Charlie, un nouveau monde s'offre à lui.",
                 'duree' => 103,
                 'annee_sortie' => '2012',
-                'lien_film' => 'https://www.youtube.com/embed/EcwoEkF5tko?si=d_C_piCsLfsv25uj',
+                'lien_film' => 'https://www.youtube.com/embed/ageKMxTF_5A?si=7xHLsVGsZEFuXVPk',
                 'lien_pochette' => 'https://th.bing.com/th/id/R.3d44e2c03a7f234f008ef2f87b90152e?rik=XZXDfjjtBo4wKg&riu=http%3a%2f%2ffr.web.img6.acsta.net%2fmedias%2fnmedia%2f18%2f93%2f52%2f64%2f20261429.jpg&ehk=mFxWdmRzbuO%2bmcxKkgf9qXOQEyOAR7YUM8jQz2FIdCE%3d&risl=&pid=ImgRaw&r=0',
                 'realisateur_id' => 30,
                 'producteur_id' => 31,
