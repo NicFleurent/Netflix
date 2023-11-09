50c742e1baa5d06666c43a60464f97653e8a3489
Mirolie10 <mirolie010@gmail.com>
2023-11-09 10:45:59 -0500

    mise de la page en forme

--- resources/views/Personnes/create.blade.php
+++ resources/views/Personnes/create.blade.php
@@ -3,49 +3,49 @@
 @section('title', 'Ajouter personnes | Netflix')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/forms.css') }}">
+<link rel="stylesheet" href="{{ asset('css/forms.css') }}">
 @endsection
 
 @section('contenu')
-    <section   class="upcoming">
+<section class="upcoming">
+    <div class="container">
         <div class="container">
-        <div class="container">
-       
-            <form class="form "method="post" action="{{route('personnes.store') }}">
-         <div class="title">Ajouter une personne</div>
+
+            <form class="form " method="post" action="{{route('personnes.store') }}">
+                <div class="title">Ajouter une personne</div>
                 @csrf
                 <div class="input-container ic1">
                     <input type="text" class="input" id="nomActeur" placeholder=" " name="nom" value="{{ old('nom')}}">
-                  <div class="cut cut-short"></div>
-                  <label for="nomActeur" class="placeholder">Nom</label>
+                    <div class="cut cut-short"></div>
+                    <label for="nomActeur" class="placeholder">Nom</label>
                 </div>
-                 @if($errors->has('nom'))
-                 <p>{{ $errors->first('nom') }}</p>
+                @if($errors->has('nom'))
+                <p>{{ $errors->first('nom') }}</p>
                 @endif
 
                 <div class="input-container ic2">
-                <input type="date" class="input" id="ddn" placeholder=" " name="date_naissance"  value="{{ old('date_naissance')}}">
-                  <div class="cut cut-ddn"></div>
-                  <label for="ddn" class="placeholder">Date de naissance</label>
+                    <input type="date" class="input" id="ddn" placeholder=" " name="date_naissance" value="{{ old('date_naissance')}}">
+                    <div class="cut cut-ddn"></div>
+                    <label for="ddn" class="placeholder">Date de naissance</label>
                 </div>
                 @if($errors->has('date_naissance'))
-                 <p>{{ $errors->first('date_naissance') }}</p>
+                <p>{{ $errors->first('date_naissance') }}</p>
                 @endif
 
                 <div class="input-container ic2">
-                <input type="text" class=" input" id="lien_photo" placeholder=" " name="lien_photo"  value="{{ old('lien_photo')}}">
-                  <div class="cut cut-lien"></div>
-                  <label for="lien_photo" class="placeholder">Lien photo de la personne</label>
+                    <input type="text" class=" input" id="lien_photo" placeholder=" " name="lien_photo" value="{{ old('lien_photo')}}">
+                    <div class="cut cut-lien"></div>
+                    <label for="lien_photo" class="placeholder">Lien photo de la personne</label>
                 </div>
                 @if($errors->has('lien_photo'))
-                 <p>{{ $errors->first('lien_photo') }}</p>
+                <p>{{ $errors->first('lien_photo') }}</p>
                 @endif
 
                 <div class="select-subgroup">
                     <label for="role">Choisir un rôle</label>
                     <div class="select select-role">
                         <select class="form-control" id="rating" name="role">
-                            <option value="" >Choisir</option>
+                            <option value="">Choisir</option>
                             <option value="Acteur" {{"Acteur" == old('role') ? 'selected' :null}}>Acteur</option>
                             <option value="Actrice" {{"Actrice" == old('role') ? 'selected' :null}}>Actrice</option>
                             <option value="Producteur" {{"Producteur" == old('role') ? 'selected' :null}}>Producteur</option>
@@ -57,12 +57,12 @@
                         </select>
                     </div>
                     @if($errors->has('role'))
-                 <p>{{ $errors->first('role') }}</p>
-                @endif
+                    <p>{{ $errors->first('role') }}</p>
+                    @endif
                 </div>
-               
+
                 <button type="submit" class="btn btn-primary">Ajouter</button>
             </form>
         </div>
-    </section>
+</section>
 @endsection
