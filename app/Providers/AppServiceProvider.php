6027b9a5097e0953a9f396cf5245b19b44108bc3
NicFleurent <fleurent.nicolas@hotmail.com>
2023-11-14 16:20:35 -0500

    Ajout middleware checkrole

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->role === $role): ?>";
+        });
+
+        Blade::directive('endrole', function () {
+            return "<?php endif; ?>";
+        });
+            
     }
 }
